255033909693d4a335c99d656afc01038fe9d32d
afsaints0987 <saints0987@gmail.com>
2023-09-10 17:20:11 +0800

    update code

--- test.js
+++ test.js
@@ -3,18 +3,6 @@ const leftButton = document.querySelector('.left-button')
 const list = document.querySelector('.list')
 let width = screen.width
 
-function updateScreen(){
-
-    if(width >= 1500){
-        leftButton.style.display = "none"
-        rightButton.style.display = "none"
-    } else {
-        leftButton.style.display = "block"
-        rightButton.style.display = "block"
-    }
-
-}
-
 rightButton.addEventListener('click', function(){
     list.scrollBy({
         left: 200,
@@ -29,9 +17,4 @@ leftButton.addEventListener('click', function(){
     })
 })
 
-updateScreen();
-
-
-list.addEventListener("scroll", updateScreen);
-window.addEventListener("resize", updateScreen)
   
